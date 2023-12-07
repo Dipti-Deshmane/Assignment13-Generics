02c88682a77957c77fd05e8d0be0dbfeaf481e13
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-12-07 14:30:49 +0530

    UC5 added

--- Findmax.java
+++ Findmax.java
@@ -4,48 +4,40 @@ public class Findmax<T extends Comparable<T>> {
     private T a;
     private T b;
     private T c;
-
-    public Findmax(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
     public <Number extends Comparable> Number maxFind(Number x,Number y ,Number z){
         Number max1 = x;
         if(y.compareTo(max1) > 0){
             max1 = y;
-        } if(z.compareTo(max1) > 0){
+        }if(z.compareTo(max1) > 0){
             max1 =z;
         }
         return max1;
     }
 
-    public T testMaximum(){
+    public T findMax(T a,T b ,T c){
         T max = a;
         if(a.compareTo(max) > 0){
             max = b;
-        } if (c.compareTo(max) > 0){
+        }  if (c.compareTo(max) > 0){
             max =c;
         }
         return max;
     }
 
     public static void main(String[] args) {
-        Findmax<Integer> maximum = new Findmax<>(1,1,1);
+        Findmax<Integer> maximum = new Findmax<>();
         System.out.println(maximum.maxFind(22,4,5));
 
-
-        Findmax<Integer> maxInt = new Findmax<>(10,20,30);
-        Integer max1 = maxInt.testMaximum();
+        Findmax<Integer> maxInt = new Findmax<>();
+        Integer max1 = maxInt.findMax(2,6,8);
         System.out.println(max1);
 
-        Findmax<Float> maxFloat = new Findmax<>(65f,11f,43f);
-        Float max2 = maxFloat.testMaximum();
+        Findmax<Float> maxFloat = new Findmax<>();
+        Float max2 = maxFloat.findMax(65f,11f,43f);
         System.out.println(max2);
 
-        Findmax<String> maxString = new Findmax<>("Apple", "Peach", "Banana");
-        String max3 = maxString.testMaximum();
+        Findmax<String> maxString = new Findmax<>();
+        String max3 = maxString.findMax("Apple", "Peach", "Banana");
         System.out.println(max3);
     }
 }
